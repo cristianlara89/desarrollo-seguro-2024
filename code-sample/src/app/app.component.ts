9fd177baf7dd95f94b513b692db02f2954f77a78
Cristian Lara <cristian89lara@gmail.com>
2024-10-02 19:25:58 -0600

    feat: agregado proyecto con configuraciones lint

--- code-sample/src/app/app.component.ts
+++ code-sample/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -6,8 +6,13 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'code-sample';
+  subtitle: string;
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
